d1af2944de9937a3e5f322af656cc27fda69fce5
dmungas <dmmungas@ucdavis.edu>
2017-02-27 19:59:05 -0800

    Dataframe or dataframe name can be input into functions.

---
+++ README.md
@@ -0,0 +1,73 @@
+recode-transform
+================
+
+This repository contains R code to recode and transform variables in a R dataframe.
+
+Transformations that are performed include:
+
+- Blom rank based inverse normal transformation   
+- Ordinal transformation using equal-quantile algorithm   
+- Ordinal transformation using equal-interval algorithm   
+  
+The formula for the Blom transformation is:   
+
+- y <- qnorm((r-c)/(N-2c+1))   
+    - where qnorm is the standard normal quantile function   
+    - r is the rank of your variable   
+    - c = 3/8, the Blom constant   
+    - N is the sample size   
+    
+Equal-quantile ordinal transformation divides the original variable into k categories based on k quantiles; the transformed variable will have a uniform distribution.
+
+Equal-interval ordinal transformation divides the original variable into k categories such that the within category range, in original variable units, is equated across categories. The distribution of the transformed variable will approximate that of the original variable. Equal-interval will start with the number of specified categories (ncat) but will iteratively decrease this number until the minimum number of observed responses (nobs) is achieved in each category.
+
+Equal-quantile is appropriate for continuous measures where the sample size divided by the
+number of categories is greater than the minimum number of observations desired for each response category. Equal-quantile does not work well with ordinal scales that have highly skewed
+distributions; equal-interval is recommended. It is important to check the distributions
+of the recoded variables to assure that the recoding matches the purpose and goals for recoding.
+
+Functions include:
+
+recodeBlom(`df, varlist_orig, varlist_tr`)  
+
+  - description: This function performs Blom transformation of selected variables   
+  - input parameters   
+    - df - label for the data frame that contains the variables to be recoded. Can be dataframe or dataframe name (in quotes).   
+    - varlist_orig - list of labels for the original variables to be recoded   
+    - varlist_tr - list of labels for the recoded variables   
+  - output: data frame that consists of the input data frame with recoded variables   
+
+recodeOrdinal(`df, varlist_orig, varlist_tr, type="interval", ncat=10, nobs=10`)    
+
+  - description: This function performs equal-quantile or equal-interval recoding into ordinal response scale   
+  - input parameters:   
+    - df - label for the data frame that contains the variables to be recoded. Can be dataframe or dataframe name (in quotes).   
+    - varlist_orig - list of labels for the original variables to be recoded   
+    - varlist_tr - list of labels for the recoded variables   
+    - type - "interval" for equal-interval recoding, "quantile" for equal-quantile   
+    - ncat - maximum number of categories for recoded variables   
+    - nobs - miminum number of observed responses in each category of the transformed variable   
+  - output: data frame that consists of the input data frame with recoded variables   
+
+recodeLookup(`df, varlist_orig, varlist_tr, type="continuous"`)  
+
+  - description: This funciton generates a reference table from recoded variables within a dataframe and uses this as a lookup table to recode vales that have not yet been recoded. The lookup values can from the "internal" input data frame or from an "external" table that has contains the original and recoded variables. "Internal" lookup is applicable where a subset of records are used for the original recode and this subset is then used as the reference to recode the other records. This function applies to continuous transformed variables (including but not limited to Blom) and to ordinal transformations.
+  - input parameters:  
+    -  df - label for the data frame that contains the variables to be recoded as well as recoded variables with recoded values for a subset of records. Can be dataframe or dataframe name (in quotes). 
+    - varlist_orig - list of labels for the original variables to be recoded   
+    - varlist_tr - list of labels for the recoded variables   
+    - type - "continuous" for continuous (numeric) transformation, "ordinal" for ordinal   
+    - lookup - NULL (no parameter input, the default) if the input table (df) will be used to lookup recode values; label for the data frame that contains the variables to be recoded if an external table is used. The external table has to be opened as a dataframe, and must contain all of the columns in varlist_orig and varlist_tr. Can be dataframe or dataframe name (in quotes).   
+  - output: data frame that consists of the input dataframe with recoded values for all records for recoded variables  
+  
+  createLookupTable(`df, varlist_orig, varlist_tr, type="continuous"`)  
+
+  - description: This function generates a reference table from recoded variables within a dataframe that can be used as a lookup table to recode vales that have not yet been recoded. This is applicable for recoded variables that take on integer values.
+  - input parameters:  
+    -  df - label for the data frame that contains the variables to be recoded as well as recoded variables with recoded values for a subset of records. Can be dataframe or dataframe name (in quotes). 
+    - varlist_orig - list of labels for the original variables to be recoded   
+    - varlist_tr - list of labels for the recoded variables   
+    - lookup - NULL (no parameter input, the default) if the input table (df) will be used to lookup recode values; label for the data frame that contains the variables to be recoded if an external table is used. The external table has to be opened as a dataframe, and must contain all of the columns in varlist_orig and varlist_tr. Can be dataframe or dataframe name (in quotes).   
+  - output: data frame that can be used as a lookup table.
+
+
---
+++ recode.R
@@ -0,0 +1,282 @@
+require(sqldf)
+require(dplyr)
+
+# Blom formula
+# y <- qnorm((r-c)/(N-2c+1))
+# where qnorm is the standard normal quantile function
+# r is the rank of your variable
+# c = 3/8, the Blom constant
+# N is the sample size
+
+# ================================ blom transformation ========================================
+
+# recodeBlom recodes original variables using a Blom rank based inverse normal transformation.
+# recodeBlom parameters:
+#   df - label for the data frame that contains the variables to be recoded.
+#       Can be dataframe or dataframe name (in quotes)
+#   varlist_orig - List of labels for the original variables to be recoded
+#   varlist_tr - List of labels for the recoded variables
+
+recodeBlom <- function(df,varlist_orig,varlist_tr){
+    if (is.data.frame(df)) {
+        tr <- df
+    } else {
+        tr <- eval(parse(text=df))
+    }
+  for (j in 1:length(varlist_orig)){
+      varlab <- varlist_tr[j]
+      tr[,varlab] <- ifelse(is.na(tr[,varlist_orig[j]]),NA,qnorm((rank(tr[,varlist_orig[j]], ties="average")-0.375)/(sum(!is.na(tr[,varlist_orig[j]])) - 2*0.375 + 1))) 
+  }
+  return(tr)
+}
+
+# ------------------------------------- end blom ----------------------------------------------
+
+# ------------------------------------ Example Code ------------------------------------------
+
+
+# ================================ recode into ordinal variables ==============================
+
+# ------------------------------- Combined Interval and Quantile ------------------------------
+
+# recodeOrdinal recodes original variables into ordinal variables using either equal-quantile 
+#   or equal-interval recoding. Equal-quantile divides the original variable into k categories
+#   based on k quantiles; the transformed variable will have a uniform distribution. Equal-interval
+#   divides the original variable into k categories with the same range the original variable units.
+#   Equal-interval will start with the number of specified categories (ncat) but will iteratively
+#   decrease this number until the minimum number of observations (nobs) is achieved in each category.
+#   Equal-quantile is appropriate for continuous measures where the sample size divided by the
+#   number of categories is greater than the minimum number of observations desired for each response
+#   category. Equal-quantile does not work well with ordinal scales that have highly skewed
+#   distributions; equal-interval is recommended. It is important to check the distributions
+#   of the recoded variables to assure that the recoding matches the purpose and goals for recoding.
+# recodeOrdinal parameters:
+#   df - label for the data frame that contains the variables to be recoded.
+#       Can be dataframe or dataframe name (in quotes).
+#   varlist_orig - List of labels for the original variables to be recoded
+#   varlist_tr - List of labels for the recoded variables
+#   type - "interval" for equal-interval recoding, "quantile" for equal quantile
+#   ncat - maximum number of categories for recoded variables
+#   nobs - miminum number of observed responses in each category of the transformed variable
+
+recodeOrdinal <- function(df,varlist_orig,varlist_tr,type="interval",ncat=10, nobs=10) {
+  if (is.data.frame(df)) {
+      tro <- df
+  } else {
+      tro <- eval(parse(text=df))
+  }
+  for (j in 1:length(varlist_orig)){
+    cuts <- {}
+    min <- min(tro[,varlist_orig[j]], na.rm=TRUE)
+    max <- max(tro[,varlist_orig[j]], na.rm=TRUE)
+    ncat1 <- ncat
+    if (type == "interval"){
+      #  Trims tails by recoding the min and max values to the value
+      #  below or above which there are cumulative nobs observations
+      for (i in 1:(ncat1+1)){
+        cuts[i] <- min + ((i-1) * ((max - min)/ncat1))
+      }
+      ordt <- cut(tro[,varlist_orig[j]], breaks=cuts, include.lowest=TRUE,
+                  labels=c(1:ncat1))
+      to <- as.data.frame(table(ordt)) 
+      to$ordt <- as.numeric(as.character(to$ordt))
+      to$cutpt <- cuts[2:length(cuts)]
+      frstrow <- c(0,0,cuts[1])
+      to <- rbind(frstrow,to)
+      cumn <- 0
+      for (k in nrow(to):1){
+        cumn <- cumn + to[k,"Freq"]
+        if (cumn >= nobs){
+          high <- to[k-1,"cutpt"]
+          break
+        }
+      }
+      cumn <- 0
+      for (k in 1:nrow(to)){
+        cumn <- cumn + to[k,"Freq"]
+        if (cumn >= nobs){
+          low <- to[k-1,"cutpt"]
+          break
+        }
+      }
+      tvar <- paste(varlist_orig[j],"t",sep="")
+      tro[,tvar] <- ifelse(tro[,varlist_orig[j]] > high,high,
+                           tro[,varlist_orig[j]])       
+      tro[,tvar] <- ifelse(tro[,tvar] < low,low,
+                           tro[,tvar])       
+      #  ------------------end trim tails-----------------         
+      cuts <- {}
+      min <- min(tro[,tvar], na.rm=TRUE)
+      max <- max(tro[,tvar], na.rm=TRUE)
+      go <- TRUE
+      repeat{
+        for (i in 1:(ncat1+1)){
+          cuts[i] <- min + ((i-1) * ((max - min)/ncat1))
+        }
+        ordt <- cut(tro[,tvar], breaks=cuts, include.lowest=TRUE,
+                    labels=c(1:ncat1))
+        #                 ordt <- cut(tro[,varlist_orig[j]], breaks=cuts, include.lowest=TRUE,
+        #                       labels=c(1:ncat1))
+        if (min(table(ordt)) >= nobs | ncat1 == 2) {go <- FALSE}
+        if (go==FALSE) {break}
+        ncat1 <- ncat1-1
+        cuts <- {}
+      }
+      varlab <- varlist_tr[j]
+      tro[,varlab] <- as.numeric(cut(tro[,tvar], breaks=cuts, 
+          include.lowest=TRUE))
+      tro <- tro[,!(names(tro) %in% tvar)]
+    } else {
+      for (i in 1:(ncat1+1)){
+        if (i==1){
+          cuts[i] <- min
+        } else{
+          cuts[i] <- quantile(tro[,varlist_orig[j]],(i-1) * (1.0/ncat1), na.rm=TRUE)
+        }  
+      }
+      cuts <- unique(cuts)
+      varlab <- varlist_tr[j]
+      tro[,varlab] <- as.numeric(cut(tro[,varlist_orig[j]], breaks=cuts, 
+          include.lowest=TRUE))
+    }
+  }
+  return(tro)
+}
+
+# -------------------------------- end ordinal recode --------------------------------------- 
+
+
+# =================================== Recode Lookup ========================================= 
+
+# recodeLookup generates a reference table from recoded variables within a dataframe and uses
+#   this as a lookup table to recode vales that have not yet been recoded. This is applicable
+#   where a subset of records are used for the original recode and this subset is then used 
+#   as the reference to recode the other records. It is applicable for continuous transformed variables
+#   (including but not limited to Blom) and for ordinal transformations.
+# recodeLookup parameters:
+#   df - label for the data frame that contains the variables to be recoded.
+#       Can be dataframe or dataframe name (in quotes).
+#   varlist_orig - List of labels for the original variables to be recoded
+#   varlist_tr - List of labels for the recoded variables
+#   type - "continuous" for continuous (numeric) transformation, "ordinal" for ordinal
+#   lookup - NULL if the input table (df) will be used to lookup recode values.
+#       Can be dataframe or dataframe name (in quotes). 
+#     that contains the variables to be recoded (in quotes) if an external table is used. The external
+#     table has to be opened as a dataframe, and must contain all of the columns 
+#     in varlist_orig and varlist_tr.
+
+#   df <- "adni_imag"
+#   varlist_orig <- blomvarin
+#   varlist_tr <- blomvarout
+#   type <- "continuous"
+#   lookup <- NA
+
+recodeLookup <- function(df,varlist_orig,varlist_tr,type="continuous",lookup=NULL) {
+    if (is.data.frame(df)){
+        rcd <- df
+    } else {
+        rcd <- eval(parse(text=df))
+    }
+     if (is.data.frame(lookup)) {
+        rcdlu <- lookup[,c(varlist_orig,varlist_tr)]
+    } else {
+        rcdlu <- eval(parse(text=lookup))
+        rcdlu <- rcdlu[,c(varlist_orig,varlist_tr)]
+    }
+    for (j in 1:length(varlist_orig)){
+        t5 <- unique(rcdlu[!is.na(rcdlu[,varlist_tr[j]]),c(varlist_orig[j],varlist_tr[j])])
+        t5 <- t5[order(t5[varlist_orig[j]]),]
+        luv <- as.data.frame(cbind(t5,rbind(t5[2:nrow(t5),],c(NA,NA))))
+        colnames(luv) <- c("orig_min","tr_min","orig_max","tr_max")
+        mino <- min(luv[,"orig_min"], na.rm=TRUE)
+        mint <- min(luv[,"tr_min"], na.rm=TRUE)
+        maxo <- max(luv[,"orig_max"], na.rm=TRUE)
+        maxt <- max(luv[,"tr_max"], na.rm=TRUE)
+        
+        #     mino <- min(rcdlu[,varlist_orig[j]], na.rm=TRUE)
+        #     mint <- min(rcdlu[,varlist_tr[j]], na.rm=TRUE)
+        #     maxo <- max(rcdlu[,varlist_orig[j]], na.rm=TRUE)
+        #     maxt <- max(rcdlu[,varlist_tr[j]], na.rm=TRUE)
+        #    cat(colnames(rcd))
+        rcd[,varlist_tr[j]] <- ifelse(rcd[,varlist_orig[j]] <= mino,mint,NA)
+         rcd[,varlist_tr[j]] <- ifelse(rcd[,varlist_orig[j]] >= maxo,maxt,rcd[,varlist_tr[j]])
+        t3 <- rcd[,c(varlist_orig[j],varlist_tr[j])]
+
+        sqlcd <- paste("SELECT * FROM t3 AS t3
+                       LEFT JOIN
+                       (SELECT * FROM luv)
+                       AS luv1 ON t3.",varlist_orig[j],
+                       " >= luv1.orig_min AND t3.", varlist_orig[j],
+                       " < luv1.orig_max",sep="")
+        t4 <- sqldf(sqlcd)
+        if (type == "continuous") {
+            t4[,varlist_tr[j]] <- ifelse(!is.na(t4[,varlist_tr[j]]),t4[,varlist_tr[j]],
+                ifelse(t4[,varlist_orig[j]] >= maxo,maxt,
+                ( ( (t4[,varlist_orig[j]] - t4$orig_min) / (t4$orig_max - t4$orig_min) ) *
+                (t4$tr_max - t4$tr_min) ) + t4$tr_min))
+            #       t4[,varlist_tr[j]] <- ifelse(!is.na(t4[,varlist_tr[j]]),t4[,varlist_tr[j]],( ( (t4[,varlist_orig[j]] - t4$orig_min) / (t4$orig_max - t4$orig_min) ) * (t4$tr_max - t4$tr_min) ) + t4$tr_min)
+        } else {
+            t4[,varlist_tr[j]] <- ifelse(!is.na(t4[,varlist_tr[j]]),t4[,varlist_tr[j]],t4$tr_min)
+        }
+        rcd[,varlist_tr[j]] <- t4[,varlist_tr[j]]
+    }
+    return(rcd) 
+}
+
+# t4[t4[,varlist_orig[j]] %in% 328,]
+# 
+#   t4[!is.na(t4[,varlist_orig[j]]) & t4[,varlist_orig[j]] > 211,]
+# summary(t4$tau)
+                                                      
+# ------------------------------------ end recode lookup --------------------------------------
+
+
+# =============================createLookupTable ====================================
+
+#   createLookupTable generates a reference table from recoded variables within a dataframe that
+#   can be used as a lookup table to recode vales that have not yet been recoded. 
+#   This is applicable for recoded variables that take on integer values.
+#   createLookupTable parameters:
+#   df - label for the data frame that contains the variables to be recoded.
+#       Can be dataframe or dataframe name (in quotes).
+#   varlist_orig - List of labels for the original variables that were recoded
+#   varlist_tr - List of labels for the recoded variables
+#     the input table (df) has to be opened as a dataframe, and must contain  
+#     all of the columns in varlist_orig and varlist_tr.
+#   lookup - NULL if the input table (df) will be used to lookup recode values.
+#       Can be dataframe or dataframe name (in quotes). 
+#   This function returns the look-up reference table in the form of a data frame.
+
+createLookupTable <- function(df,varlist_orig,varlist_tr,lookup=NULL) {
+    if (is.data.frame(df)) {
+        rcd <- df
+    } else {
+        rcd <- eval(parse(text=df))
+    }
+    if (is.data.frame(lookup)) {
+        rcdlu <- lookup[,c(varlist_orig,varlist_tr)]
+    } else {
+        rcdlu <- eval(parse(text=lookup))
+        rcdlu <- rcdlu[,c(varlist_orig,varlist_tr)]
+    }
+  for (j in 1:length(varlist_orig)){
+    t5 <- unique(rcdlu[!is.na(rcdlu[,varlist_tr[j]]),c(varlist_orig[j],varlist_tr[j])])
+    t5 <- t5[order(t5[varlist_orig[j]]),]
+    colnames(t5) <- c("orig","recode_score")
+    t6 <- t5 %>% group_by(recode_score) %>%
+      summarise(
+        min = min(orig),
+        max = max(orig)
+      )
+    colnames(t6) <- gsub("min",paste("min_",varlist_orig[j],sep=""),colnames(t6))
+    colnames(t6) <- gsub("max",paste("max_",varlist_orig[j],sep=""),colnames(t6))
+    if (j == 1){
+      luv <- t6
+    } else {
+      luv <- merge(luv,t6,by="recode_score",all.x=TRUE,all.y=TRUE)
+    }
+  }
+  return(luv) 
+}
+
+# --------------------------end createLookupTable -----------------------------------
